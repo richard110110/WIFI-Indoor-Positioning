86032f48e821a48b077ef6d5fe5450a0ebdc1ee4
Richard <cguo3889@uni.sydney.edu.au>
2021-08-04 10:01:56 +1000

    add intersection

--- app/src/main/java/com/example/week2module/ListAdapter.java
+++ app/src/main/java/com/example/week2module/ListAdapter.java
@@ -174,4 +174,6 @@ public class ListAdapter extends BaseAdapter {
         double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(signalLevelInDb)) / 20.0;
         return Math.pow(10.0, exp);
     }
+
+
 }
